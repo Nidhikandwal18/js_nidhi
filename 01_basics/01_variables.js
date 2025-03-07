850d8a851a02048b60b11d2c4bfa75dfbbab1759
Nidhikandwal18 <147969811+Nidhikandwal18@users.noreply.github.com>
2025-03-07 06:02:11 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId =1144553
+let accountEmail="nidhikandwal79@gmail.com"
+var accountPasswd="1234"
+accountCity="Jaipur"
+accountEmail="niiidhii@gmail.com"
+accountCity="Noida"
+// accountId=2 not allowed
+console.table([accountId,accountEmail,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; //treat all code as new version
+let name="nidhi";
+let age=23;
+let isGirl=true;
+console.table([name,age,isGirl]);
